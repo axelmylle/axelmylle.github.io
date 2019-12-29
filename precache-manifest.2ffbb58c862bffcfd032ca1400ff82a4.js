8c646b0aab8e036a9c0d7706a475919b075d3309
Axel Mylle <axelmylle@MacBook-Pro-van-Axel.local>
2019-12-29 01:17:21 +0100

    Updates

---
+++ precache-manifest.2ffbb58c862bffcfd032ca1400ff82a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe62be3fbc1795f60542d328cd558c8",
+    "url": "/axelmylle.github.io/index.html"
+  },
+  {
+    "revision": "7ae05c63e461973b77aa",
+    "url": "/axelmylle.github.io/static/css/main.1ac4de6f.chunk.css"
+  },
+  {
+    "revision": "ea363725807abf577bc3",
+    "url": "/axelmylle.github.io/static/js/2.3fc36edb.chunk.js"
+  },
+  {
+    "revision": "7ae05c63e461973b77aa",
+    "url": "/axelmylle.github.io/static/js/main.0e5da34d.chunk.js"
+  },
+  {
+    "revision": "6afc3990656f729ef11a",
+    "url": "/axelmylle.github.io/static/js/runtime~main.72195ac8.js"
+  },
+  {
+    "revision": "ec7d6fff2537e63ca217d9fe33a09a45",
+    "url": "/axelmylle.github.io/static/media/Proxima-Nova-Bold.ec7d6fff.woff"
+  },
+  {
+    "revision": "a33fdc2e1c4b2f1e66ddaf443b407b0f",
+    "url": "/axelmylle.github.io/static/media/Proxima-Nova-Regular.a33fdc2e.woff"
+  },
+  {
+    "revision": "342d46faf3d11fef4387dec4f2a41e4d",
+    "url": "/axelmylle.github.io/static/media/Proxima-Nova-Semibold.342d46fa.woff"
+  },
+  {
+    "revision": "864622624490e59d39dd0e6afa978411",
+    "url": "/axelmylle.github.io/static/media/greek-freak.86462262.svg"
+  }
+]);
